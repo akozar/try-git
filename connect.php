04e95fab85e54ebfdf12ee7d3e3fee1b73530186
guessforme <klavier34@gmail.com>
2016-04-06 19:39:43 +0200

    Added connect.php to repository

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+	$db_database = 'bendmachine';
+	$db_hostname = 'localhost';
+	$db_username = 'root';
+	$db_password = '';
+	$db_server=mysqli_connect($db_hostname,$db_username,$db_password,$db_database);
+?>
